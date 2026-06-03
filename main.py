6c75a0304d2091cf2cc53aba3840e324cd926f76
Adam <adam.blake@brainlabsdigital.com>
2026-06-03 14:39:19 -0300

    add row validation logic and utils.py import function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # 1. Find and open the file
 import csv
+from utils import validate_row
 file_name='dirty_data.csv'
 
 print("---- Validating Each Row ----")
@@ -12,28 +13,9 @@ with open(file_name, mode='r', newline='', encoding='utf-8') as csv_file:
 # 3. For each row of data in the file:
     for row in csv_reader:
 
-        # assuming all rows are valid until proven otherwise
-        is_row_valid = True
+        # pulling function from utils.py file
+        is_row_valid, clicks, spend = validate_row(row)
 
-        # Rule 1: Name, Spend, CLick Fields cannot be emtpy
-        if not row['Campaign Name'] or not row['Clicks'] or not row['Spend']:
-            is_row_valid = False
-            
-
-        if is_row_valid:
-            try:
-                # Rule 2: clicks and spend must be numeric so we try ot convert them to decimal numbers
-                # if they cant be converted then they are not a number and it fails and goes to except block
-                clicks = float(row['Clicks'])
-                spend = float(row['Spend'])
-
-                # Rule 3 :clicks and spend cannot be negative
-                if clicks < 0 or spend < 0:
-                    is_row_valid = False
-                    
-
-            except ValueError:
-                is_row_valid = False
 
         if is_row_valid:
             campaign_name = row['Campaign Name']
